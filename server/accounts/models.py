5a50cd40fb614fd035c15dcfb351410a97603f61
박홍은 <tkfkd1909@naver.com>
2020-05-20 23:39:31 +0900

    he | ADD Dog, Breed model S02P31B201-10

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,8 +1,27 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+
+class Breed(models.Model):
+    name = models.CharField(max_length=50)
+    name_en = models.CharField(max_length=50)
+    origin = models.CharField(max_length=50)
+    group = models.CharField(max_length=50)
+    min_height = models.FloatField()
+    max_height = models.FloatField()
+    min_weight = models.FloatField()
+    max_weight = models.FloatField()
+
+class Dog(models.Model):
+    breed = models.ForeignKey(to=Breed, on_delete=models.PROTECT)
+    device = models.IntegerField()
+    name = models.CharField(max_length=50)
+    profile = models.ImageField()
+    birthyear = models.IntegerField()
+
 class User(AbstractUser):
     email = models.EmailField(
         max_length=255,
         unique=True
     )
+    dogs = models.ManyToManyField(to=Dog)
--- server/accounts/utils/EmailAuthBackend.py
+++ server/accounts/utils/EmailAuthBackend.py
@@ -1,22 +1,22 @@
 from django.contrib.auth import get_user_model
-from accounts.models import User
-
-class EmailAuthBackend(object):
+from django.contrib.auth.backends import ModelBackend
+UserModel = get_user_model()
+class EmailAuthBackend(ModelBackend):
     """
-    유저네임 대신 이메일로 인증을 수행하는 백엔드 모듈
+                    
+    Username 대신 email로 인증을 수행하는 백엔드 모듈
     """
-    def authenticate(self, request=None, username=None, password=None):
-        """ 인증에 성공하면 user 오브젝트를, 실패하면 None을 리턴 """
+    def authenticate(self, request, email=None, password=None, **kwargs):
+        if email is None:
+            email = kwargs.get(UserModel.EMAIL_FIELD)
+        if email is None or password is None:
+            return
         try:
-            user = User.objects.get(email=username)
-            if user.check_password(password):
+            user = UserModel._default_manager.get_by_natural_key(email)
+        except UserModel.DoesNotExist:
+            # Run the default password hasher once to reduce the timing
+            # difference between an existing and a nonexistent user (#20760).
+            UserModel().set_password(password)
+        else:
+            if user.check_password(password) and self.user_can_authenticate(user):
                 return user
-        except get_user_model().DoesNotExist:
-            return None
-
-    def get_user(self, user_id):
-        """ 유저아이디에서 User객체를 호출 """
-        try:
-            return User.objects.get(pk=user_id)
-        except get_user_model().DoesNotExist:
-            return None
