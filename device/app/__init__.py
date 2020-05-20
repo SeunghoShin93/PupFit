81a891959e7d37591b6cbeb62eb13db8eb1bda84
woodg1207 <woodg1207@gmail.com>
2020-05-20 09:53:13 +0900

    dg | temp set flask S02P31B201-16

--- device/app/__init__.py
+++ device/app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
-from flask_restplus import Api
 
 app = Flask(__name__)
 
-api = Api(app, version='1.0', title='라즈베리파이 api', description='센서값들을 모아서 aws로 보내주는 api')
-
-# import app.views
 from app import views
--- device/app/views.py
+++ device/app/views.py
@@ -1,14 +1,20 @@
-# from flask import current_app
-from flask_restplus import Api, Resource, fields
-# import api
+from app import app
+from flask_restx import Resource, Api
 
-@api.route('/test')
-def hello():
-    return hello
+api = Api(app,version='1.0', title='Rsap for Pupfit', description='라즈베리파이에서 센서값 받고 aws로 정제해서 보내주는 API')
 
-# @api.route('/')
-# def index():
-#     return {'message':'test'}
-# class InputManage(Resource):
-#     def get(self):
-#         return {'message':'Test swagger'}
+@api.route('/hello')
+@api.doc(params={'test':'tt'})
+class HelloWorld(Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+
+@api.route('/serial')
+# @api.doc(params={'serial_num':'str'})
+class SerialNumber(Resource):
+    def get(self):
+        seri = {
+            'serial_num':'hongsi',
+        }
+        return seri
