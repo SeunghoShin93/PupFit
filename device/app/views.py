6e563f4a693b5207175e7ec70ecf6919f739f4a4
woodg1207 <woodg1207@gmail.com>
2020-05-21 18:51:45 +0900

    dg | temp add flask api S02P31B201-39

--- device/app/views.py
+++ device/app/views.py
@@ -1,5 +1,7 @@
+from flask import request
 from app import app
 from flask_restx import Resource, Api
+
 
 api = Api(app,version='1.0', title='Rsap for Pupfit', description='라즈베리파이에서 센서값 받고 aws로 정제해서 보내주는 API')
 hongsi = 'pupfithonsi'
@@ -19,3 +21,18 @@ class SerialNumber(Resource):
         }
         return seri
         
+@api.route('/heartrate')
+@api.doc(params={'test':'test'})
+class HeartRate(Resource):
+    def post(self):
+        print(request.data)# json으로 요청올때. 근데 bytes 타입으로 온다..
+        # d = request.data
+        ##이런형식으로 써야하는구나 
+        f = request.form # dict형식이면 이걸로.
+        # print(f['test'])
+        print(f)
+        data = {
+            'test':'dummy',
+
+        }
+        return data
