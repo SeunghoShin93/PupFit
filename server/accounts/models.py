0a3ec1106c6cd5a9798c5c4a101bdd37458dce82
박홍은 <tkfkd1909@naver.com>
2020-05-21 19:48:37 +0900

    he | ADD device_api, dog_apply_api S02P31B201-10

--- server/accounts/models.py
+++ server/accounts/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+class Device(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
 
 class Breed(models.Model):
     name = models.CharField(max_length=50)
@@ -14,7 +16,7 @@ class Breed(models.Model):
 
 class Dog(models.Model):
     breed = models.ForeignKey(to=Breed, on_delete=models.PROTECT)
-    device = models.IntegerField()
+    device = models.OneToOneField(Device, on_delete=models.PROTECT, primary_key=True)
     name = models.CharField(max_length=50)
     profile = models.ImageField()
     birthyear = models.IntegerField()
@@ -24,6 +26,6 @@ class User(AbstractUser):
         max_length=255,
         unique=True
     )
-    dogs = models.ManyToManyField(to=Dog)
+    dogs = models.ManyToManyField(to=Dog, related_name='owners')
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
