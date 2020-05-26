077590abd6bef43c90201650b208b2de5151068a
woodg1207 <woodg1207@gmail.com>
2020-05-26 16:49:11 +0900

    dg | ADD post api read db(sqlite3)

--- device/app/views.py
+++ device/app/views.py
@@ -1,3 +1,4 @@
+import sqlite3
 from flask import request
 from app import app
 from flask_restx import Resource, Api
@@ -5,13 +6,6 @@ from flask_restx import Resource, Api
 
 api = Api(app,version='1.0', title='Rsap for Pupfit', description='라즈베리파이에서 센서값 받고 aws로 정제해서 보내주는 API')
 hongsi = 'pupfithonsi'
-@api.route('/hello')
-# @api.doc(params={'test':'tt'})
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
-
-
 @api.route('/serial')
 # @api.doc(params={'serial_num':'str'})
 class SerialNumber(Resource):
@@ -25,14 +19,24 @@ class SerialNumber(Resource):
 @api.doc(params={'test':'test'})
 class HeartRate(Resource):
     def post(self):
-        print(request.data)# json으로 요청올때. 근데 bytes 타입으로 온다..
+        #print(request.data)# json으로 요청올때. 근데 bytes 타입으로 온다..
         # d = request.data
         ##이런형식으로 써야하는구나 
         f = request.form # dict형식이면 이걸로.
         # print(f['test'])
-        print(f)
-        data = {
-            'test':'dummy',
+        con = sqlite3.connect('db.sqlite3')
+        con.row_factory = sqlite3.Row
+        cur = con.cursor()
+        data = {}        
+        for idx, row in enumerate(cur.execute('SELECT * FROM dog_status')):
+            data[f'info{idx}'] = {
+                'id':row[0],
+                'datetime':row[1],
+                'heartrate':row[2],
+                'temperature':row[3],
+                'gpslat':row[4],
+                'gpslon':row[5],
+            }
 
-        }
+
         return data
