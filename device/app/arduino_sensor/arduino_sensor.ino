b64790bcf575cf86fb55c0d132268c9b8a534c07
woodg1207 <woodg1207@gmail.com>
2020-05-31 14:34:56 +0900

    dg | TEMP for rasp not pull request

--- device/app/arduino_sensor/arduino_sensor.ino
+++ device/app/arduino_sensor/arduino_sensor.ino
@@ -17,7 +17,7 @@ void setup(){
   Wire.endTransmission(true);
 
   Serial.begin(9600);
-  Serial.println("X,Y,Z");
+//  Serial.println("X,Y,Z");
 
 }
 
@@ -65,94 +65,3 @@ void loop(){
   delay(200);
 
 }
-
-
-//#include <Wire.h>
-//
-//#include "MAX30105.h"  //Get it here: http://librarymanager/All#SparkFun_MAX30105
-//#include "heartRate.h"
-//MAX30105 particleSensor;
-//
-//// 심박수.
-//const byte RATE_SIZE = 4; //Increase this for more averaging. 4 is good.
-//byte rates[RATE_SIZE]; //Array of heart rates
-//byte rateSpot = 0;
-//long lastBeat = 0; //Time at which the last beat occurred
-//
-//float beatsPerMinute;
-//int beatAvg;
-//int cnt = 0;
-//
-//
-//void setup() {
-//  // put your setup code here, to run once:
-//  Serial.begin(115200);
-//  Serial.println("Initializing...");
-//
-//    if (particleSensor.begin(Wire, I2C_SPEED_FAST) == false) //Use default I2C port, 400kHz speed
-//  {
-//    Serial.println("MAX30105 was not found. Please check wiring/power. ");
-//    while (1);
-//  }
-//  //온도
-//  //The LEDs are very low power and won't affect the temp reading much but
-//  //you may want to turn off the LEDs to avoid any local heating
-//  //particleSensor.setup(0); //Configure sensor. Turn off LEDs
-//  //particleSensor.setup(); //Configure sensor. Use 25mA for LED drive
-//  //심박
-//  particleSensor.setup(); //Configure sensor with default settings
-//  particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-//  particleSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
-//  //온도
-//  particleSensor.enableDIETEMPRDY(); //Enable the temp ready interrupt. This is required.
-//}
-//
-//void loop() {
-//  float temperature = particleSensor.readTemperature();
-//  
-//
-//
-//  cnt++ ;
-//  
-//  long irValue = particleSensor.getIR();
-//
-//  if (checkForBeat(irValue) == true)
-//  {
-//    //We sensed a beat!
-//    long delta = millis() - lastBeat;
-//    lastBeat = millis();
-//
-//    beatsPerMinute = 60 / (delta / 1000.0);
-//
-//    if (beatsPerMinute < 255 && beatsPerMinute > 20)
-//    {
-//      rates[rateSpot++] = (byte)beatsPerMinute; //Store this reading in the array
-//      rateSpot %= RATE_SIZE; //Wrap variable
-//
-//      //Take average of readings
-//      beatAvg = 0;
-//      for (byte x = 0 ; x < RATE_SIZE ; x++)
-//        beatAvg += rates[x];
-//      beatAvg /= RATE_SIZE;
-//    }
-//  }
-//  Serial.print("temperatureC=");
-//  Serial.print(temperature, 4);
-////  Serial.println();
-//  if(cnt%1==0){
-//    cnt = 0;
-//    
-//    Serial.print("IR=");
-//    Serial.print(irValue);
-//    Serial.print(", BPM=");
-//    Serial.print(beatsPerMinute);
-//    Serial.print(", Avg BPM=");
-//    Serial.print(beatAvg);
-//
-//    if (irValue < 50000)
-//      Serial.print(" No finger?");
-//    Serial.println();
-//  }
-//  
-//
-//}
--- device/app/sensor.py
+++ device/app/sensor.py
@@ -1,4 +1,5 @@
 import sqlite3
+import serial
 import time
 import logging
 from logging.handlers import RotatingFileHandler
@@ -6,26 +7,53 @@ from logging.handlers import RotatingFileHandler
 conn = sqlite3.connect('db.sqlite3')
 db = conn.cursor()
 db.execute('''
-    CREATE TABLE IF NOT EXISTS dog_status (
+    CREATE TABLE IF NOT EXISTS dog_gps (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         datetime DATETIME DEFAULT (datetime('now','localtime')),
-        heartrate FLOAT,
-        temperature FLOAT,
-        gpslat FLOAT,
-        gpslon FLOAT
+        connect BOOLEAN DEFAULT FALSE,
+        gpslat FLOAT DEFAULT 0,
+        gpslon FLOAT DEFAULT 0
     );
-    ''')
+    ''') ### gps는 초단위로 들어오게 connect 변수는 커넥트가 안될때.
+db.execute('''
+    CREATE TABLE IF NOT EXISTS dog_accel (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        datetime DATETIME DEFAULT (datetime('now','localtime')),
+        x INT,
+        y INT,
+        z INT
+    );
+''') # 1초당 들어오는 값들의 평균이 저장된다. 임시적으로...음.. 근데 그럼..어쩌지...
 print('running...')
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-file_handler = RotatingFileHandler("logs/data.log", mode='a', maxBytes=1024, backupCount=10, encoding=None, delay=0)
-streamHandler = logging.StreamHandler()
-logger.addHandler(file_handler)
-logger.addHandler(streamHandler)
+# logger = logging.getLogger(__name__)
+# logger.setLevel(logging.DEBUG)
+# file_handler = RotatingFileHandler("logs/data.log", mode='a', maxBytes=1024, backupCount=10, encoding=None, delay=0)
+# streamHandler = logging.StreamHandler()
+# logger.addHandler(file_handler)
+# logger.addHandler(streamHandler)
 
-
+ardu = serial.Serial('COM8',9600)
+cnt = 0
 while 1:
+    x = []
+    y = []
+    z = []
+    tm = time.localtime()
+    start = tm.tm_sec
+    while start+1>time.localtime().tm_sec:
+        a = ardu.readline()
+        b = a.decode()[:-2]
+        c=list(map(int,b.split(',')))
+        x.append(c[0])
+        y.append(c[1])
+        z.append(c[2])
+        # print(tm.tm_sec)
+        if start==59 and time.localtime().tm_sec==0:
+            break
+    x_range = abs(max(x)-min(x))
+    y_range = abs(max(y)-min(y))
+    z_range = abs(max(z)-min(z))
     try:
         #sensor로 데이터 받아오기 
         db.execute(f''' 
@@ -33,9 +61,9 @@ while 1:
         VALUES (80, 26, 38.898, 70.002);
         ''')
         conn.commit()
-        t= time.strftime("%Y-%m-%d %I:%M:%S", time.localtime())
-        logger.info("data inserted"+ t)
-        time.sleep(5)
+        # t= time.strftime("%Y-%m-%d %I:%M:%S", time.localtime())
+        # logger.info("data inserted"+ t)
+        # time.sleep(5)
     except Exception as e:
-        logger.error(e)        
+        # logger.error(e)        
         break 
