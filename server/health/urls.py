d44c3cd8238828bb0feddb611f2c63198a537fab
박홍은 <tkfkd1909@naver.com>
2020-06-08 10:23:55 +0900

    he | ADD weekly_data api

--- server/health/urls.py
+++ server/health/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:device_id>/rasp/', views.rasp),
     path('<int:dog_id>/walking/', views.walking_active),
-    path('weeklydata/', views.weekly_data)
+    path('weeklydata/', views.weekly_data),
     path('<int:dog_id>/walking/list/', views.waliking_list),
     path('<int:dog_id>/walking/list/<int:walk_id>', views.wlking_detail),
 ]
